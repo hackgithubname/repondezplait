08112b852823c9a5810552df4392c1886511ef59
Krzysztof Baranowski <pharcosyle@gmail.com>
2014-11-05 16:10:08 -0800

    first!

---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject delta-auto "0.1.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [environ "1.0.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.3.1"]
+                 [hiccup "1.0.5"]
+                 [garden "1.2.1"]
+                 [xhh/clang "0.1.0-SNAPSHOT"]]
+  :min-lein-version "2.0.0" ; For Heroku.
+  :main delta-auto.server.server
+  :plugins [[environ "1.0.0"]
+            ;; [lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.11"]
+            ]
+  ;; :hooks [leiningen.cljsbuild]
+  ;; :cljsbuild {:builds [{:source-paths ["src/delta_auto/app"]
+  ;;                       :compiler {:output-to "output/app.js"
+  ;;                                  :optimizations :whitespace
+  ;;                                  :pretty-print true}}]}
+  :ring {:handler delta-auto.server.server/application
+         :port 5000
+         :auto-refresh? true}
+  :profiles {:production {:env {:production true}}}
+  :env {:asdf 454545})
