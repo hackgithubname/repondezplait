24e3ab57901b2a9bb2e162c0582783146994541b
Krzysztof Baranowski <pharcosyle@gmail.com>
2014-11-12 18:54:17 -0800

    alas

--- src/repondezplait/server.clj
+++ src/repondezplait/server.clj
@@ -37,7 +37,7 @@
                   new-body #(->> (next raw-body-lines) (join %) (trim))
                   new-body-text (new-body "\n")
                   oid (ObjectId.)
-                  new-message {:from "repondezplait@gmail.com"
+                  new-message {:from sender ; It doesn't matter what this is set to, Gmail will override it (but keeps the proper name, if any).
                                :reply-to sender
                                :to recipient
                                ;; :to (.getRecipients message javax.mail.Message$RecipientType/TO)
