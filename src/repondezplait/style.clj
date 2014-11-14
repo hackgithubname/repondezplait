e881a7a682df122c00b863786cb1a3af1660fc0a
Krzysztof Baranowski <pharcosyle@gmail.com>
2014-11-14 00:12:32 -0800

    stuff

--- src/repondezplait/style.clj
+++ src/repondezplait/style.clj
@@ -9,35 +9,16 @@
 (def stylesheet
   (let [white "white"
 
-        wet-asphalt "#34495E"
-        midnight-blue "#2C3E50"
-
-        clouds "#ECF0F1"
-        silver "#BDC3C7"
-
-        concrete "#95A5A6"
-        asbestos "#7F8C8D"
-
-        turquoise "#1ABC9C"
-        green-sea "#16A085"
-
-        emerald "#2ECC71"
-        nephritis "#27AE60"
-
-        peter-river "#3498DB"
-        belize-hole "#2980B9"
-
-        amethyst "#9B59B6"
-        wisteria "#8E44AD"
-
-        sunflower "#F1C40F"
-        orange "#F39C12"
-
-        carrot "#E67E22"
-        pumpkin "#D35400"
-
-        alizarin "#E74C3C"
-        pomegranite "#C0392B"
+        wet-asphalt "#34495E" midnight-blue "#2C3E50"
+        clouds "#ECF0F1" silver "#BDC3C7"
+        concrete "#95A5A6" asbestos "#7F8C8D"
+        turquoise "#1ABC9C" green-sea "#16A085"
+        emerald "#2ECC71" nephritis "#27AE60"
+        peter-river "#3498DB" belize-hole "#2980B9"
+        amethyst "#9B59B6" wisteria "#8E44AD"
+        sunflower "#F1C40F" orange "#F39C12"
+        carrot "#E67E22" pumpkin "#D35400"
+        alizarin "#E74C3C" pomegranite "#C0392B"
 
         text-font "'Lato', sans-serif"]
     (css
